f8cfa7ff79fc2c472863a1ac30ba6747f189a7bb
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-05 00:52:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,3 @@
 require 'bundler/setup'
 Bundler.require(:default, :development)
 
-# source "https://rubygems.org"
-# gem "rspec"
-# gem "sinatra"
-#
-#
-#
-# group :development do
-#   gem "pry"
-# end
